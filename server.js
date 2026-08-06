4eb6db44455163f00f89480569a3d652e7364223
Vinnyvinny2 <vincent@crojungle.com>
2026-08-06 10:33:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
-
+ 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
